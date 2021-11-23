650a8e15f8c33d1424ee5881b3220b68ab2ccd5b
Petr Surovtsev <petr.surovcev@gmail.com>
2021-11-23 15:54:08 +0300

    add settings.py создан класс Settings внесены соответствующие изменения в alien-invasion.py

--- alien-invasion.py
+++ alien-invasion.py
@@ -1,18 +1,19 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
-    # Инициализирует игру и создает объект экрана.
+    # Инициализирует pygame, settings и объект экрана.
     pygame.init()
-    screen = pygame.display.set_mode((400, 400))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    # Назначение цвета фона
-    bg_color = (230, 230, 230)
 
     # Запуск основного цикла
     while True:
         # При каждом проходе цикла перерисовывается экран
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # Отслеживание событий клавиатуры и мыши
         for event in pygame.event.get():
---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """Класс для хранения всех настроек игры Alien Invasion"""
+
+    def __init__(self):
+        """Инициализируем настройки игры"""
+        # Параметры экрана
+        self.screen_width = 400
+        self.screen_height = 400
+        self.bg_color = (200, 200, 200)
